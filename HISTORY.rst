41d406f4e3d3f8f978cad65154ff21410e84b802
Daniela Rabe <dara@gfz-potsdam.de>
2022-11-08 15:27:56 +0100

    update version and HISTORY.rst

--- HISTORY.rst
+++ HISTORY.rst
@@ -2,6 +2,13 @@
 History
 =======
 
+0.1.1 (2022-11-08)
+------------------
+
+* Clean config file and config.py
+* fix some bugs
+* added config check and _cli
+
 0.1.0 (2022-11-03)
 ------------------
 
--- s2downloader/version.py
+++ s2downloader/version.py
@@ -19,5 +19,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.1.0'
-__versionalias__ = '2022-11-03_01'
+__version__ = '0.1.1'
+__versionalias__ = '2022-11-08_01'
