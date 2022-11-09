2098937940669b2ef1ee267b299c00b772e05fa1
Daniela Rabe <dara@gfz-potsdam.de>
2022-11-09 09:00:18 +0100

    prepare test class

--- tests/test_s2downloader.py
+++ tests/test_s2downloader.py
@@ -20,23 +20,67 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import json
+import os
+import shutil
+import unittest
+
+import numpy
 import pytest
+import rasterio
+
+from rasterio.crs import CRS
+from s2downloader.s2downloader import s2DataDownloader
+from s2downloader.config import loadConfiguration, Config
+from copy import deepcopy
 
 
-from s2downloader import s2downloader  # noqa: F401 (imported but unused)
+class TestSentinel2Portal(unittest.TestCase):
+    root_path = None
+    config_file = None
+    configuration = None
+    output_data_path = None
 
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.root_path = "./"
+        if os.path.basename(os.getcwd()) == "tests":
+            cls.root_path = "../"
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+        cls.config_file = os.path.abspath(f"{cls.root_path}data/default_config.json")
+        cls.configuration = loadConfiguration(path=cls.config_file)
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+        cls.configuration['user_settings']['result_settings']['results_dir'] = "tests/temp_results"
 
+        cls.output_data_path = cls.configuration['user_settings']['result_settings']['results_dir']
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+        if not os.path.exists(cls.output_data_path):
+            try:
+                os.mkdir(cls.output_data_path)
+            except OSError:
+                print(f"Creation of test data output directory {cls.output_data_path} failed")
+                raise
+        else:
+            raise Exception(f'Test directory {cls.output_data_path} already exists!')
+
+    @classmethod
+    def tearDown(cls) -> None:
+        # delete testfolder
+        try:
+            if os.path.exists(cls.output_data_path):
+                shutil.rmtree(cls.output_data_path)
+        except OSError:
+            print("Deletion of the directory %s failed" % cls.output_data_path)
+        else:
+            print("Successfully deleted the directory %s" % cls.output_data_path)
+
+    def testS2DownloaderDefault(self):
+        """Test configuration default settings."""
+
+        config = deepcopy(self.configuration)
+
+        Config(**config)
+        s2DataDownloader(config_dict=config)
+
+        # check output
+        
