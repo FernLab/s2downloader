aceb2de13535123366e22097c24e40beeba60c2c
Romulo <romulo@gfz-potsdam.de>
2022-11-23 13:15:03 +0100

    Remove license text

--- s2downloader/version.py
+++ s2downloader/version.py
@@ -1,23 +1,5 @@
 # -*- coding: utf-8 -*-
 """Version module for S2Downloader."""
 
-# Downloader for Sentinel-2 data.
-#
-# Copyright (c) 2022, FernLab (GFZ Potsdam, fernlab@gfz-potsdam.de)
-#
-# This software was developed within the context [...]
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 __version__ = '0.2.0'
 __versionalias__ = '2022-11-25_01'
