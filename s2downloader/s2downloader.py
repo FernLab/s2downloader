7740d6903dc1b416e9e57b5cc3ad956d1726e7c7
Romulo <romulo@gfz-potsdam.de>
2024-06-13 18:15:57 +0200

    URL should be a string.

--- s2downloader/s2downloader.py
+++ s2downloader/s2downloader.py
@@ -474,7 +474,7 @@ def downloadTileID(*, config_dict: dict):
                                 bb=None,
                                 date_range=aoi_settings['date_range'],
                                 props_json=tile_settings,
-                                stac_catalog_url=s2_settings['stac_catalog_url'],
+                                stac_catalog_url=str(s2_settings['stac_catalog_url']),
                                 logger=logger)
 
     data_msg = []
